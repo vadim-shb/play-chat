4282836f2494703e720b8472492d206e030c2938
vdshb <vadim.shabanov@gmail.com>
2016-02-12 09:41:51 +0300

    Main page works

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,10 +1,14 @@
 package controllers
 
 import actors.ConnectionActor
-import play.api.mvc.{WebSocket, Controller}
+import play.api.mvc.{Action, WebSocket, Controller}
 import play.api.Play.current
 
 object Application extends Controller {
+
+  def indexRedirect = Action {
+    Redirect("/index.html")
+  }
 
   def wsHandler = WebSocket.acceptWithActor[String, String] { request => out => {
     ConnectionActor.props(out)
